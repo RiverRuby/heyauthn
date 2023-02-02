b6d1059b5b6c98d11a8841dfd9504467ee946c61
Vivek Bhupatiraju <vivekab@mit.edu>
2023-02-02 13:01:38 -0500

    fixed API bugs

--- frontend/pages/api/rep.ts
+++ frontend/pages/api/rep.ts
@@ -1,35 +1,36 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from "next"
+import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
-async function query(id: String) {
-    const user = await prisma.user.findMany({
-        where: { id: id }
-      })
+async function query(id: string) {
+  const user = await prisma.user.findUnique({
+    where: { id: id },
+  })
 
-    const increaseRep = await prisma.user.update({
-      where: { id: id },
-      data: { reputation: user.reputation },
-    })
-    console.log(increaseRep)
-  }
+  const increaseRep = await prisma.user.update({
+    where: { id: id },
+    data: { reputation: user.reputation },
+  })
+  console.log(increaseRep)
+}
 
 export default function handler(
   request: NextApiRequest,
-  response: NextApiResponse,
+  response: NextApiResponse
 ) {
-    const {body} = request
-    const b = JSON.parse(body).id
+  const { body } = request
+  const b = JSON.parse(body).id
 
-    query(b).then(async () => {
-        await prisma.$disconnect()
-      })
-      .catch(async (e) => {
-        console.error(e)
-        await prisma.$disconnect()
-        process.exit(1)
-      })
+  query(b)
+    .then(async () => {
+      await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+      console.error(e)
+      await prisma.$disconnect()
+      process.exit(1)
+    })
 
-  response.status(200);
+  response.status(200)
 }
