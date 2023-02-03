e0b5fc27f82bdd588148ea9ad5d93a3bc78e156d
Vivek Bhupatiraju <vivekab@mit.edu>
2023-02-03 08:44:35 -0500

    cleaned up navbar, added logo to pages, added back button on questions page to discord page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,16 +1,16 @@
 import { useState } from "react"
 import Head from "next/head"
+import Image from "next/image"
 import { useSemaphore } from "@/contexts/SemaphoreProvider"
 import Balancer from "react-wrap-balancer"
 
 import { Layout } from "@/components/layout"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 
 export default function IndexPage() {
   const [username, setUsername] = useState("")
-  const { handleAuthenticate, handleRegister } = useSemaphore()
+  const { handleRegister } = useSemaphore()
   return (
     <Layout>
       <Head>
@@ -20,14 +20,15 @@ export default function IndexPage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className="container flex h-screen flex-col items-center justify-center gap-6 pt-6 pb-8 md:py-10">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-          heyauthn!
-        </h1>
+        <div className="flex justify-center">
+          <Image src="/heyauthn.png" alt="heyauthn!" width="225" height="162" />
+        </div>
         <div className="wrap flex flex-col gap-6">
           <Input
+            className="text-center"
             onChange={(e) => setUsername(e.target.value)}
             value={username}
-            placeholder="Username"
+            placeholder="Name"
           />
           <Button onClick={() => handleRegister(username)}>Register</Button>
         </div>
