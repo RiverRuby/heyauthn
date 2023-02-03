82d6115b7f5966d1644f9e401aa84a5b89033d9f
Sehyun Chung <sehyun@berkeley.edu>
2023-02-03 00:55:15 -0800

    try out topbar

--- frontend/components/main-nav.tsx
+++ frontend/components/main-nav.tsx
@@ -22,7 +22,7 @@ interface MainNavProps {
 export function MainNav({ items }: MainNavProps) {
   return (
     <div className="flex gap-6 md:gap-10">
-      <Link href="/" className="hidden items-center space-x-2 md:flex">
+      {/* <Link href="/" className="hidden items-center space-x-2 md:flex">
         <Icons.logo className="h-6 w-6" />
         <span className="hidden font-bold sm:inline-block">
           {siteConfig.name}
@@ -46,7 +46,7 @@ export function MainNav({ items }: MainNavProps) {
               )
           )}
         </nav>
-      ) : null}
+      ) : null} */}
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button
@@ -54,10 +54,10 @@ export function MainNav({ items }: MainNavProps) {
             className="-ml-4 text-base hover:bg-transparent focus:ring-0 md:hidden"
           >
             <Icons.logo className="mr-2 h-4 w-4" />{" "}
-            <span className="font-bold">Menu</span>
+            <span className="font-bold">heyauthn!</span>
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent
+        {/* <DropdownMenuContent
           align="start"
           sideOffset={24}
           className="w-[300px] overflow-scroll"
@@ -76,7 +76,7 @@ export function MainNav({ items }: MainNavProps) {
                 </DropdownMenuItem>
               )
           )}
-        </DropdownMenuContent>
+        </DropdownMenuContent> */}
       </DropdownMenu>
     </div>
   )
