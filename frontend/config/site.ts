6b300915ae0e921980cd7d2090a57486f6aa337d
Vivek Bhupatiraju <vivekab@mit.edu>
2023-02-03 08:45:13 -0500

    saved file

--- frontend/config/site.ts
+++ frontend/config/site.ts
@@ -21,14 +21,8 @@ export const siteConfig: SiteConfig = {
     },
   ],
   links: {
-<<<<<<< HEAD
     twitter: "https://twitter.com/heyanonxyz",
     github: "https://github.com/vb7401/heyauthn",
     docs: "https://github.com/vb7401/heyauthn",
-=======
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/vb7401/heyauthn",
-    docs: "https://ui.shadcn.com",
->>>>>>> 9364f8bf78dc5c6c196fd18565cdcd39d07157f0
   },
 }
